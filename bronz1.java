51e4f9b6761af8cceba0176b08795d8102ad3487
emreArvas <120271442+emreArvas@users.noreply.github.com>
2023-01-26 13:45:31 +0300

    Update bronz1.java

--- bronz1.java
+++ bronz1.java
@@ -8,3 +8,8 @@ public class Main {
         }
         System.out.println(toplam);
     }}
+//10'un altındaki 3 veya 5'in katı olan tüm doğal sayıları sıralarsak 3, 5, 
+6 ve 9 elde ederiz. Bu katların toplamı 23'tür.
+1000'in altındaki 3 veya 5'in tüm katlarının toplamını bulan Java kodunu 
+yazınız.
+   
