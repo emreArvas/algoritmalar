896bb8b62292668d172a411191bda590a3ed9751
emreArvas <120271442+emreArvas@users.noreply.github.com>
2023-01-26 13:41:58 +0300

    Create bronz1.java

---
+++ bronz1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int toplam=0;
+        for (int i = 0; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+               toplam=toplam+i;
+            }
+        }
+        System.out.println(toplam);
+    }}
